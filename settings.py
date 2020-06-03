5a9ed96b259439e98f235b0a2f8f1b76fa3444dd
Rian <53636103+Rian-c-132@users.noreply.github.com>
2020-06-03 22:27:02 +0600

    added new settings

--- settings.py
+++ settings.py
@@ -1,11 +1,13 @@
 class Settings:
 	def __init__(self):
-		self.screen_width = 720
-		self.screen_height = 500
+		self.screen_width = 1000
+		self.screen_height = 620
 		self.bg_color = (230, 230, 230)
 		self.ship_speed = 1.4;
-
+		self.alien_speed = 1.0
 		self.bullet_speed = 1.0
 		self.bullet_width = 3
 		self.bullet_height = 15
+		self.fleet_drop_speed = 3.0
+		self.fleet_direction = 1
 		self.bullet_color = (160, 60, 60)
